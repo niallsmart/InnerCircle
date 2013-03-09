bce9957217db7fde82a7b2faf70ec825876d8cc6
Niall Smart <niall@pobox.com>
2013-03-09 10:31:10 -0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InnerCircle::Application.config.secret_token = 'a929c4e87632d34565c879c1253bdb3666e07abd4e2cda7767047de421e8b9a103dff4a7898c3721015d59b6997944fbe13e487c664f21cd9111487c9083b0fd'
